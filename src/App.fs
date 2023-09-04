5675dab4dfaae8cde30a6f908aec23f9aa060d46
Luca Hoffmann <luca.hoffmann@msu-solutions.de>
2023-09-04 09:09:59 +0200

    increase delayed

--- src/App.fs
+++ src/App.fs
@@ -17,3 +17,18 @@ decrease.onclick <- fun ev ->
     countViewer.innerText <- sprintf "Count is at %d" currentCount
 
 countViewer.innerText <- sprintf "Count is at %d" currentCount
+
+let runAfter (ms: int) callback =
+    async {
+        do! Async.Sleep ms
+        do callback()
+    }
+    |> Async.StartImmediate
+
+let increaseDelayed = document.getElementById("increaseDelayed")
+
+increaseDelayed.onclick <- fun _ ->
+    runAfter 1000 (fun () ->
+        currentCount <- currentCount + 1
+        countViewer.innerText <- sprintf "Count is at %d" currentCount
+    )
